0c734bf725b0731292fd17a5f403127963302f86
nori <nori@nori-no-MacBook-Air.local>
2017-05-13 21:11:00 +0900

    json形式に保存

---
+++ save_csv_jpin.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+print('rank,city,population')
+print(','.join(['1','上海','24150000']))
+print(','.join(['2','カラチ','23150000']))
+print(','.join(['3','北京','22150000']))
+print(','.join(['4','天津','21150000']))
